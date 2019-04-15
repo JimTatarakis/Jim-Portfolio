6148a06ddeb7363f0fef912fb71951d3c3c16ed3
Jim Tatarakis <jimtatarakis@gmail.com>
2019-04-15 03:30:22 -0700

    Template Complete

---
+++ assets/javascript/app.js
@@ -0,0 +1,20 @@
+// Sidenav
+const sideNav = document.querySelector('.sidenav');
+M.Sidenav.init(sideNav, {});
+
+// Slider
+const slider = document.querySelector('.slider');
+M.Slider.init(slider, {
+    indicators: false,
+    height: 500,
+    transition: 500,
+    interval: 7000
+})
+
+// Materialboxed
+const mb = document.querySelectorAll('.materialboxed');
+M.Materialbox.init(mb, {});
+
+// Scrollspy
+const ss = document.querySelectorAll('.scrollspy');
+M.ScrollSpy.init(ss, {});
